fa863207aa0a405fac553dce5587445d7cca9ba5
Gabriel Marques <marquesgabriel03@outlook.com>
2024-02-26 19:39:54 -0300

    print demonstration

---
+++ wowprints.go
@@ -0,0 +1,18 @@
+package main
+
+import ("fmt")
+
+func main(){
+	var str1 string = "roku bun"
+	var str2 string = "no ichi"
+	
+	fmt.Print("This is a print with newline using 'backslash n'\n")
+	fmt.Println("This is a print using newline from Println")
+	fmt.Printf("This is a print using prinft: %v", str1)
+	fmt.Println(" " ,str2)
+}
+
+/*
+Also, var can be used inside/outside functions.
+Inferring works only inside a function
+*/
